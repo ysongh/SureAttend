25330f1f24d46e1efa48e506f80c3d608f2e6e12
You Song Hou <ysongweb3@gmail.com>
2024-06-16 01:40:57 -0400

    refactor: change the title

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useCallback, useRef, useState } from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Bars3Icon, BugAntIcon, CalendarDaysIcon } from "@heroicons/react/24/outline";
@@ -102,16 +102,16 @@ export const Header = () => {
           className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0"
         >
           <div className="flex relative w-10 h-10">
-            <Image
+            {/* <Image
               alt="SE2 logo"
               className="cursor-pointer"
               fill
               src="/logo.svg"
-            />
+            /> */}
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-Stark</span>
-            <span className="text-xs">Starknet dev stack</span>
+            <span className="font-bold leading-tight">Sure Attend</span>
+            <span className="text-xs"></span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
